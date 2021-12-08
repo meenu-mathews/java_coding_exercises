487bb8086df91ba6faf4690da9092914588d3782
Meenu Maria Mathews <38442902+meenu-mathews@users.noreply.github.com>
2021-12-08 14:31:00 +0000

    Last function in Ex1 WIP, Ex 2-5 yet to start

--- src/main/java/com/techreturners/exercise001/Exercise001.java
+++ src/main/java/com/techreturners/exercise001/Exercise001.java
@@ -5,26 +5,34 @@ import java.util.List;
 public class Exercise001 {
     public String capitalizeWord(String word) {
         // Add your code here
-        return "";
+        System.out.println(Character.toUpperCase(word.charAt(0)) + word.substring(1));
+        return Character.toUpperCase(word.charAt(0)) + word.substring(1);
     }
 
     public String generateInitials(String firstName, String lastName) {
         // Add your code here
-        return "";
+        System.out.println(Character.toUpperCase (firstName.charAt(0))+"." +Character.toUpperCase (lastName.charAt(0)));
+        return Character.toUpperCase (firstName.charAt(0))+"."+Character.toUpperCase (lastName.charAt(0));
     }
 
     public double addVat(double originalPrice, double vatRate) {
         // Add your code here
-        return 0.0;
+        System.out.println(Math.round((originalPrice+(originalPrice*(vatRate/100)))*100.0)/100.0);
+        return Math.round((originalPrice+(originalPrice*(vatRate/100)))*100.0)/100.0;
     }
 
     public String reverse(String sentence) {
         // Add your code here
-        return "";
+        StringBuilder str = new StringBuilder();
+        for(int i=sentence.length()-1;i>=0;i--){
+            str = str.append(sentence.charAt(i));
+        }
+        System.out.println(str);
+        return str.toString();
     }
 
     public int countLinuxUsers(List<User> users) {
         // Add your code here
-        return 0;
+        return users.size();
     }
 }
