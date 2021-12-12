066bc45fdfeba1cc02f9b1c65f71444d27fc7fc9
Meenu Maria Mathews <38442902+meenu-mathews@users.noreply.github.com>
2021-12-12 22:25:33 +0000

    EX5 missing letter tests yet to complete

--- src/main/java/com/techreturners/exercise005/Exercise005.java
+++ src/main/java/com/techreturners/exercise005/Exercise005.java
@@ -1,9 +1,17 @@
 package com.techreturners.exercise005;
-
 public class Exercise005 {
 
     public boolean isPangram(String input) {
-        throw new UnsupportedOperationException("You can delete this statement and add your code here.");
+        System.out.println(input.length());
+        if(input.length()==0 || input.equals("null"))
+            return false;
+        for (int i = 0; i < input.length(); i++)
+        {
+              if(input.matches("^[a-zA-Z]*$"))
+                return true;
+
+        }
+        return true;
     }
 
 }
